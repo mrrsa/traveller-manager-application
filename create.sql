3cae13ec1d6a14a8126046454eb77f204da3c74a
Marta Silva <martaraquel90@gmail,com>
2020-10-28 11:52:27 +0000

    add application properties prod

--- create.sql
+++ create.sql
@@ -1046,3 +1046,19 @@
         modelo varchar(255),
         primary key (matricula)
     ) engine=InnoDB
+
+    create table vehicle (
+       matricula varchar(255) not null,
+        consumo_combinado varchar(255),
+        marca varchar(255),
+        modelo varchar(255),
+        primary key (matricula)
+    ) engine=InnoDB
+
+    create table vehicle (
+       matricula varchar(255) not null,
+        consumo_combinado varchar(255),
+        marca varchar(255),
+        modelo varchar(255),
+        primary key (matricula)
+    ) engine=InnoDB
